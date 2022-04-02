f5944747f1e6daa7a006f5596cf2507de0f2bb3a
ramii19780529 <ramii19780529@gmail.com>
2022-04-02 04:02:06 -0400

    Small change to format.

--- arduino/rocket/convert.py
+++ arduino/rocket/convert.py
@@ -3,10 +3,14 @@ import csv
 
 with open("DATALOG.CSV", "w", newline="") as file_out:
     csv_out = csv.writer(file_out)
-    csv_out.writerow(("Millisecond", "XL_X", "XL_Y", "XL_Z", "G_X", "G_Y", "G_Z", "P", "T"))
+    csv_out.writerow(
+        ("Millisecond", "XL_X", "XL_Y", "XL_Z", "G_X", "G_Y", "G_Z", "P", "T")
+    )
     with open("DATALOG.DAT", mode="rb") as file_in:
         while True:
             chunk = file_in.read(36)
             if not chunk:
                 break
-            csv_out.writerow([i if i == i else "" for i in struct.unpack("fffffffff", chunk)])
+            csv_out.writerow(
+                [i if i == i else "" for i in struct.unpack("fffffffff", chunk)]
+            )
